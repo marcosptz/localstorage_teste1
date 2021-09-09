8b606cd0b7cf305b363e337ef6dc802cf1fca5b5
marcosptz <petzingermarcos@gmail.com>
2021-09-09 01:26:59 -0300

    first commit

--- script.js
+++ script.js
@@ -1,47 +1,32 @@
-var gcorf, gcort, gnome, n, teste;
+var gqtd, gcomp, glarg, gdesc;
 
+// Verifica se o browser tem suporte ao localStorage
 if (typeof (Storage) != 'undefined') {
-    document.write('Tudo ok !');
+    document.write('Suporte a Web Storage Existente !');
 } else {
     document.write('Sem suporte a Web Storage !');
 }
 
+// Salvar no localStorage
 function salvar(){
-    gnome = document.getElementById('nome').value;
-    teste = document.getElementById('teste').value;
-    // localStorage.corf=gcorf;
-    // localStorage.cort=gcort;
-    localStorage.nome=gnome;
-    localStorage.teste=teste;
-
-    let dados = [
-        {"teste":teste,"nome":gnome}
-    ]
-
-    dados_json=JSON.stringify(dados)
-    // console.log(dados_json)
-    localStorage.dados=dados_json
-    n = localStorage.dados
-    var y = n = n
-    console.log(y)
-    // alert(n);
-    // document.querySelector("#btnCapturarDados").addEventListener("click", ()=>{
-    //     document.querySelector("#containerDados").innerHTML = JSON.stringify(y, undefined, 4);
-    // });
+    gqtd = document.getElementById('qtd').value;
+    gcomp = document.getElementById('comp').value;
+    glarg = document.getElementById('larg').value;
+    gdesc = document.getElementById('desc').value;
+    localStorage.qtd=gqtd;
+    localStorage.comp=gcomp;
+    localStorage.larg=glarg;
+    localStorage.desc=gdesc;
 }
 
-function defineCor(op, cor){
-    if(op==1) {
-        document.body.style.backgroundColor=cor;
-        gcorf = cor;
-    } else {
-        document.body.style.color=cor;
-        gcort = cor;
-    }
-
-}
-
+// Exibe os valores no formulario
 function exibir(){
-    var dados = localStorage.getItem('dados');
-    document.write(dados);
+    var qtd = document.getElementById('qtd');
+    qtd.value=localStorage.qtd;
+    var comp = document.getElementById('comp');
+    comp.value=localStorage.comp;
+    var larg = document.getElementById('larg');
+    larg.value=localStorage.larg;
+    var desc = document.getElementById('desc');
+    desc.value=localStorage.desc;
 }
